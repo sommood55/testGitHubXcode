505ef38e55eff192754b1a9b3c65dafbc9659d30
Som Mood <sommood55@gmail.com>
2012-09-01 11:50:37 +0700

    First Commit

---
+++ testGitHubXcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGitHubXcode
+//
+//  Created by boonyanit.m on 9/1/55 BE.
+//  Copyright (c) 2555 boonyanit.m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGitHubXcode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testGitHubXcode
+//
+//  Created by boonyanit.m on 9/1/55 BE.
+//  Copyright (c) 2555 boonyanit.m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
